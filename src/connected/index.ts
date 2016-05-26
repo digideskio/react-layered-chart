84d926cbb2d10077c2cc4fc9534dffdfb2453193
Sean Kelley <seansfkelley@gmail.com>
2016-05-25 22:32:06 -0700

    Add index file. Delete loaders, which are not appropriate for this library.

--- src/connected/index.ts
+++ src/connected/index.ts
@@ -1,5 +1,3 @@
-export { Range, ScaleFunction, DataPoint, DataBucket, Stack } from 'react-layered-chart';
-
 export { default as XAxisLayer, OwnProps as XAxisLayerProps } from './components/XAxisLayer';
 export { default as LayerCake, Props as LayerCakeProps } from './components/LayerCake';
 export { default as StackedSeriesLayer, OwnProps as StackedSeriesLayerProps } from './components/StackedSeriesLayer';
@@ -8,8 +6,6 @@ export { default as InteractionLayer, OwnProps as InteractionLayerProps } from '
 export { default as HoverLayer, OwnProps as HoverLayerProps } from './components/HoverLayer';
 export { default as ResizeSentinelLayer } from './components/ResizeSentinelLayer';
 export { default as LayerType } from './model/LayerType';
-export { default as epochLoader } from './flux/epochLoader';
-export { default as computedChannelsLoader } from './flux/computedChannelsLoader';
 export * from './export-only/exportableActions';
 export * from './export-only/exportableSelectors';
 export * from './export-only/exportableState';

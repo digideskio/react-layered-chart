53af70bcae7e9a086b75d1c54ab319e883f16ca5
Sean Kelley <seansfkelley@gmail.com>
2016-05-25 22:23:31 -0700

    Add tsconfig and attendant libraries/typings.

---
+++ extra-typings.d.ts
@@ -0,0 +1,8 @@
+declare var process: { env: { NODE_ENV: string } };
+
+// I don't depend on the d3 typings because as of this writing they're way out of date and I don't want to rewrite all
+// of the scale typings for a single method right now.
+declare module 'd3-scale' {
+  var scale: any;
+  export = scale;
+}
